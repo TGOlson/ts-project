81a8af532f4213d9484867074307fbd5b7ca03bd
Tyler Olson <tydotg@gmail.com>
2024-01-04 13:11:02 -0800

    lint and ts config tweaks

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,10 +16,10 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-unused-vars': ['warn', { 'varsIgnorePattern': '^_', 'argsIgnorePattern': '^_' }],
     '@typescript-eslint/semi': 'warn',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/consistent-indexed-object-style': 'off',
     // Used on previous projects. Don't need these rules, but some may be preventing bugs?
     // 'semi': 'off',
     // '@typescript-eslint/non-nullable-type-assertion-style': 'off',
-    // '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
-    // '@typescript-eslint/consistent-indexed-object-style': 'off'
   },
 };
