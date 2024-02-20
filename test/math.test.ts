42ecde6850363f74ebd65fffbb619b720ffcb16f
Tyler Olson <tydotg@gmail.com>
2024-02-20 08:49:58 -0800

    test update

--- test/math.test.ts
+++ test/math.test.ts
@@ -10,8 +10,13 @@ describe('math', () => {
     test('sums a list of numbers', () => {
       expect(sum([1, 2, 5, 10])).toBe(18);
     });
+    
     test('works with an empty list', () => {
       expect(sum([])).toBe(0);
     });
+
+    test('array matchers', () => {
+      expect([[1, 2], [3, 4, 5], [6, 7, 8, 9]].map(sum)).toEqual([3, 12, 30]);
+    })
   });
 });
